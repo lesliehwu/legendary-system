77032279474e7644908465658811cf5a838dff85
lesliehwu <mslesliewu@gmail.com>
2017-10-24 17:06:34 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title="Retro Barcode Generator";
+    barcode=new Array(10);
+    colors=['pink','lightpink','hotpink','deeppink','palevioletred','mediumvioletred','peachpuff','thistle'];
+    rand(){
+        return (Math.floor(Math.random()*7));
+    }
 }
